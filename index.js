aace7b7cfa99758ed60e132126d61ca66b95f3ab
Septian Maulana <septianm028@gmail.com>
2022-06-26 09:45:39 +0700

    [tianbuyung] - added update function (based on price, name, quantity), modified delete function, added OOP Inheritance

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ class Menu {
       console.log("Berikut menu yang ditambahkan :", menus[menus.length - 1]);
       fs.writeFile("./data.json", JSON.stringify(menus), (err) => {
         if (err) throw err;
+        fs.readFile("./data.json", "utf-8", (err, data) => {
+          if (err) console.log("data tidak ditemukan");
+          const newMenus = JSON.parse(data);
+          const menusFiltered = newMenus.filter((menu) => {
+            return menu.status !== false;
+          });
+          console.log("berikut menu yang tersedia: ", menusFiltered);
+        });
       });
     });
   }
@@ -38,30 +46,32 @@ class Menu {
     });
   }
   // Read detail Menu with status true
-  getDetailMenu(id) {
+  getDetailMenu() {
     fs.readFile("./data.json", "utf-8", (err, data) => {
       if (err) throw err;
       const menus = JSON.parse(data);
       menus.forEach((menu) => {
-        if (menu.id === id && menu.status === true) {
+        if (menu.id === this.menu.id && menu.status === true) {
           console.log("Ini adalah menu: ", menu);
-        } else if (menu.id === id && menu.status === false) {
+        } else if (menu.id === this.menu.id && menu.status === false) {
           console.log("Menu tidak tersedia!");
         }
       });
     });
   }
   // Delete and Update status of Menu by ID
-  deleteMenu(id) {
+  deleteMenu() {
     fs.readFile("./data.json", "utf-8", (err, data) => {
       if (err) throw err;
       const menus = JSON.parse(data);
       menus.forEach((menu) => {
-        if (menu.id === id && menu.status === true) {
+        if (menu.id === this.menu.id && menu.status === true) {
           menu.status = false;
+        } else if (menu.id === this.menu.id && menu.status === false) {
+          console.log("Menu tidak tersedia!");
         }
       });
-      // console.log(menus);
+      console.log("Berikut menu yang di hapus:", menus[this.menu.id - 1]);
       fs.writeFile("./data.json", JSON.stringify(menus), (err) => {
         if (err) throw err;
         fs.readFile("./data.json", "utf-8", (err, data) => {
@@ -77,13 +87,105 @@ class Menu {
   }
 }
 
+// OOP Inheritance
+class UpdateMenu extends Menu {
+  constructor(menu) {
+    super(menu);
+    // console.log("ini adalah menu", menu);
+    this.id = menu.id;
+    // console.log("ini adalah id", menu.id);
+  }
+  // Update Price Menu by id
+  updateMenuPrice() {
+    fs.readFile("./data.json", "utf-8", (err, data) => {
+      if (err) throw err;
+      const menus = JSON.parse(data);
+      menus.forEach((menu) => {
+        if (menu.id === this.id && menu.status === true) {
+          menu.price = this.menu.price;
+          console.log("Berikut menu yang di update:", menus[this.id - 1]);
+          fs.writeFile("./data.json", JSON.stringify(menus), (err) => {
+            if (err) throw err;
+            fs.readFile("./data.json", "utf-8", (err, data) => {
+              if (err) console.log("data tidak ditemukan");
+              const newMenus = JSON.parse(data);
+              const menusFiltered = newMenus.filter((menu) => {
+                return menu.status !== false;
+              });
+              console.log("Ini adalah harga menu terbaru: ", menusFiltered);
+            });
+          });
+        } else if (menu.id === this.id && menu.status === false) {
+          console.log("Menu tidak tersedia!");
+        }
+      });
+    });
+  }
+  // Update Name Menu by id
+  updateMenuName() {
+    fs.readFile("./data.json", "utf-8", (err, data) => {
+      if (err) throw err;
+      const menus = JSON.parse(data);
+      menus.forEach((menu) => {
+        if (menu.id === this.id && menu.status === true) {
+          menu.name = this.menu.name;
+          console.log("Berikut menu yang di update:", menus[this.id - 1]);
+          fs.writeFile("./data.json", JSON.stringify(menus), (err) => {
+            if (err) throw err;
+            fs.readFile("./data.json", "utf-8", (err, data) => {
+              if (err) console.log("data tidak ditemukan");
+              const newMenus = JSON.parse(data);
+              const menusFiltered = newMenus.filter((menu) => {
+                return menu.status !== false;
+              });
+              console.log("Ini adalah harga menu terbaru: ", menusFiltered);
+            });
+          });
+        } else if (menu.id === this.id && menu.status === false) {
+          console.log("Menu tidak tersedia!");
+        }
+      });
+    });
+  }
+  // Update Quantity Menu by id
+  updateMenuQuantity() {
+    fs.readFile("./data.json", "utf-8", (err, data) => {
+      if (err) throw err;
+      const menus = JSON.parse(data);
+      menus.forEach((menu) => {
+        if (menu.id === this.id && menu.status === true) {
+          menu.quantity = this.menu.quantity;
+          console.log("Berikut menu yang di update:", menus[this.id - 1]);
+          fs.writeFile("./data.json", JSON.stringify(menus), (err) => {
+            if (err) throw err;
+            fs.readFile("./data.json", "utf-8", (err, data) => {
+              if (err) console.log("data tidak ditemukan");
+              const newMenus = JSON.parse(data);
+              const menusFiltered = newMenus.filter((menu) => {
+                return menu.status !== false;
+              });
+              console.log("Ini adalah harga menu terbaru: ", menusFiltered);
+            });
+          });
+        } else if (menu.id === this.id && menu.status === false) {
+          console.log("Menu tidak tersedia!");
+        }
+      });
+    });
+  }
+}
+
 const newMenu = new Menu({
-  name: "Nasi cha kangkung",
-  price: 14500,
-  quantity: 3,
+  name: "Ketoprak",
+  price: 13500,
+  quantity: 2,
 });
 // newMenu.addMenu();
 // newMenu.getPermanentListMenus();
 // newMenu.getListMenus();
-// newMenu.getDetailMenu(4);
-// newMenu.deleteMenu(8);
+// newMenu.getDetailMenu();
+// newMenu.deleteMenu();
+const newPrice = new UpdateMenu({ quantity: 4, id: 9 });
+// newPrice.updateMenuPrice();
+// newPrice.updateMenuName();
+// newPrice.updateMenuQuantity();
